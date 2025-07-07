77412b91e6e0da9fe9ea7b2aca3bc293d3d62287
nganesh <nganesh@iu.edu>
2025-07-07 06:14:04 -0700

    project 4

---
+++ project 4.py
@@ -0,0 +1,10 @@
+class reverse():
+    
+    def __init__(self):
+        self.default = "love island"
+    
+    def reversedefault(self):
+        print(self.default[::-1])
+    
+reverseobj = reverse()
+reverseobj.reversedefault()
